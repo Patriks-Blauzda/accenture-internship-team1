428588188b0161445fda45b570939283bfea224b
Patriks-Blauzda <91874563+Patriks-Blauzda@users.noreply.github.com>
2021-10-06 11:08:11 +0300

    Add files via upload
    Exported project

---
+++ majaslapa/src/main/java/com/example/majaslapa/MajaslapaApplication.java
@@ -0,0 +1,14 @@
+package com.example.majaslapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajaslapaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MajaslapaApplication.class, args);
+    }
+
+}
---
+++ majaslapa/src/test/java/com/example/majaslapa/MajaslapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.majaslapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajaslapaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
